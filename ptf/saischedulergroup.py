66601d2b435a6471c5152ab84e41db85c384ea5b
Aleksandra Jereczek <aleksandra.jereczek@intel.com>
2021-12-10 11:12:13 +0000

    Correct imprecise asserts

--- ptf/saischedulergroup.py
+++ ptf/saischedulergroup.py
@@ -40,7 +40,7 @@ class SchGroupParamsTest(SaiHelper):
         port_attr = sai_thrift_get_port_attribute(
             self.client, test_port, qos_number_of_scheduler_groups=True)
         schgroup_no = port_attr['qos_number_of_scheduler_groups']
-        self.assertTrue(schgroup_no != 0)
+        self.assertNotEqual(schgroup_no, 0)
 
         schgroup_list = sai_thrift_object_list_t(count=100)
         port_attr = sai_thrift_get_port_attribute(
@@ -57,7 +57,7 @@ class SchGroupParamsTest(SaiHelper):
         schgroup_attr = sai_thrift_get_scheduler_group_attribute(
             self.client, test_schgroup, child_count=True)
         child_no = schgroup_attr['child_count']
-        self.assertTrue(child_no != 0)
+        self.assertNotEqual(child_no, 0)
 
         schgroup_child_list = sai_thrift_object_list_t(count=100)
         schgroup_attr = sai_thrift_get_scheduler_group_attribute(
@@ -72,7 +72,7 @@ class SchGroupParamsTest(SaiHelper):
             sched = sai_thrift_create_scheduler(
                 self.client, meter_type=SAI_METER_TYPE_PACKETS,
                 scheduling_type=SAI_SCHEDULING_TYPE_STRICT)
-            self.assertTrue(sched != 0)
+            self.assertNotEqual(sched, 0)
 
             status = sai_thrift_set_scheduler_group_attribute(
                 self.client, test_schgroup, scheduler_profile_id=sched)
@@ -107,22 +107,22 @@ class SchGroupCrteateTest(SaiHelper):
         sched = sai_thrift_create_scheduler(
             self.client, meter_type=SAI_METER_TYPE_PACKETS,
             scheduling_type=SAI_SCHEDULING_TYPE_STRICT)
-        self.assertTrue(sched != 0)
+        self.assertNotEqual(sched, 0)
 
         try:
             sched_group = sai_thrift_create_scheduler_group(
                 self.client, port_id=test_port, scheduler_profile_id=sched)
             # Level is mandatory so creation should fail
-            self.assertTrue(sched_group == 0)
+            self.assertEqual(sched_group, 0)
 
             sched_group = sai_thrift_create_scheduler_group(
                 self.client, level=0, scheduler_profile_id=sched)
             # Port is mandatory so creation should fail
-            self.assertTrue(sched_group == 0)
+            self.assertEqual(sched_group, 0)
 
             sched_group = sai_thrift_create_scheduler_group(
                 self.client, port_id=test_port, level=0)
-            self.assertTrue(sched_group != 0)
+            self.assertNotEqual(sched_group, 0)
 
         finally:
             sai_thrift_remove_scheduler(self.client, sched_group)
